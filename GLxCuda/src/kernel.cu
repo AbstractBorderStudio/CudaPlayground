1cd69b6b9e2f68eb9dc25bd7280fb5def620a978
Daniel Bologna <89863727+AbstractBorderStudio@users.noreply.github.com>
2024-01-31 13:10:47 +0100

    triangle spinning

--- GLxCuda/src/kernel.cu
+++ GLxCuda/src/kernel.cu
@@ -70,11 +70,49 @@ void FPSCounter();
  *              MAIN
 ***************************************/
 
+// global variables
 static GLFWwindow* window;
 static cudaError_t cudaStatus;
 
 static double lastTime;
 static int nFrames;
+
+// data
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.5f, -0.5f, 1.f, 0.f, 0.f },
+    {  0.5f, -0.5f, 0.f, 1.f, 0.f },
+    {   0.f,  .5f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    // send the vertex color in output for the fragment shader to use\n"
+"    color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    // use the vertex color from the vertex buffer\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
+
+static GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+static GLint mvp_location, vpos_location, vcol_location;
 
 int main(void)
 {
@@ -82,6 +120,34 @@ int main(void)
     Init();
     // create window
     CreateWindow();
+
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+        sizeof(vertices[0]), (void*)0);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+        sizeof(vertices[0]), (void*)(sizeof(float) * 2));
 
     // rendering loop
     RenderLoop(window);
@@ -107,10 +173,10 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
-    //else if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
-    //{
-    //    test << <10, 1 >> > ();
-    //}
+    else if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+    {
+        test << <10, 1 >> > ();
+    }
 }
 
 /**************************************
@@ -119,13 +185,15 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 static void Init()
 {
+    // set glfw error callback
     glfwSetErrorCallback(error_callback);
 
+    // init glfw
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);    
 
     // init fps counter
     lastTime = 0;
@@ -134,17 +202,22 @@ static void Init()
 
 static void CreateWindow()
 {
-    window = glfwCreateWindow(480, 600, "GLxCuda", NULL, NULL);
+    // create window
+    window = glfwCreateWindow(600, 600, "GLxCuda", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
+    // set glfw window callbacks
     glfwSetKeyCallback(window, key_callback);
 
+    // create context
     glfwMakeContextCurrent(window);
+    // load OpenGL functionalities
     gladLoadGL();
+    // set
     glfwSwapInterval(1);
 }
 
@@ -158,20 +231,34 @@ void RenderLoop(GLFWwindow* _window)
     {
         FPSCounter();
 
-        // calculate screen ratio to scale content with the window
-        //float ratio;
+        float ratio;
         int width, height;
+        mat4x4 m, p, mvp;
 
-        glfwGetFramebufferSize(_window, &width, &height);
-        //ratio = width / (float)height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
 
+        // tell OpenGl the window size ratio
         glViewport(0, 0, width, height);
+        //glViewport(0, 0, 100, 100);
 
         // clear screen for the next frame
         glClear(GL_COLOR_BUFFER_BIT);
-
         // color screen
         glClearColor(0.1, 0.5, 1.0, 1.0);
+
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        
+
+        
 
         // swap buffer and pull events
         glfwSwapBuffers(_window);
